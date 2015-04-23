a5da84177e574a5f0a13fcb0e453496b29fedbd8
flipoffaclif <flipoffaclif@gmail.com>
2015-04-23 15:02:28 -0500

    title saver
    title saver

--- bat version/file saver.bat
+++ bat version/file saver.bat
@@ -1,6 +1,7 @@
 @echo off
 :loop
 dir M:\media_location  /b/s > d:\media_list.txt
+
 cls
 echo Created by: flipoffaclif
 echo 	email: flipoffaclif@gmail.com
@@ -12,12 +13,9 @@ echo titles of all of your media to a txt file,
 echo in a seperate location such as a usb drive
 echo or sd card
 echo ------------------------------------------------------
-echo In the config.ini you can change the
-echo 	Media location
-echo 	List output folder
-echo 	Backup frequency
-echo ------------------------------------------------------
-timeout 360 /NOBREAK
+
+REM change "#" to how many seconds it will take before repeating
+timeout # /NOBREAK
 )
 cls
 goto loop
